4c0860e977c8a364c2e26877ef0c4a25348e6aae
Jon Choukroun <jon.choukroun@twenty20.com>
2017-01-27 15:21:31 -0800

    Implement masonry after page load

--- js/masonry.js
+++ js/masonry.js
@@ -1,6 +1,8 @@
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  gutter: '.gutter-sizer',
-  percentPosition: true
+$(window).load(function() {
+  $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    gutter: '.gutter-sizer',
+    percentPosition: true
+  });
 });
