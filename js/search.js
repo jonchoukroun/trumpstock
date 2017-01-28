5d15c09e22d0dc4fdf5344daac23d742ad3e96be
Jon Choukroun <jonchoukroun@gmail.com>
2017-01-27 19:35:28 -0800

    Allow typing in search field

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.form-group').on('keypress', 'input', function(e) {
-    e.preventDefault();
     if (e.which === 13) {
+      e.preventDefault();
       document.location.href = './empty_search.html';
     }
   });
